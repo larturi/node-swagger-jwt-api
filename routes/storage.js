13e48187ef178979bb6e98ac4a0472dd61b1d9de
Leandro Arturi <lea.arturi@gmail.com>
2022-10-10 20:07:20 -0300

    Swagger

--- routes/storage.js
+++ routes/storage.js
@@ -4,12 +4,108 @@ const { getItem, getItems, createItem, deleteItem } = require("../controllers/st
 const { validateId } = require("../validators/storage");
 const { upload } = require("../utils/handleStore");
 
-router.post("/", upload.single("file"), createItem);
+/**
+* @swagger
+* components:
+*   schemas:
+*     Storage:
+*       type: object
+*       properties:
+*         url:
+*           type: string
+*         filename:
+*           type: string
+*/
 
+/**
+* @swagger
+* /storage:
+*   post:
+*     summary: Upload file
+*     security:
+*       - bearerAuth: []
+*     tags: [Storage]
+*     requestBody:
+*       content:
+*         multipart/form-data:
+*           schema:
+*             type: object
+*             properties:
+*               file:
+*                 type: string
+*                 format: binary
+*     responses:
+*       default:
+*         description: successful operation
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Storage'
+*/
+
+router.post("/", upload.single("file"), createItem);
+/**
+* @swagger
+* /storage:
+*   get:
+*     summary: Upload a file
+*     tags: [Storage]
+*     responses:
+*       default:
+*         description: successful operation
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Storage'
+*/
 router.get("/", getItems);
 
+/**
+* @swagger
+* /storage/{id}:
+*   get:
+*     summary: Get one Storage File by id
+*     tags: [Storage]
+*     responses:
+*       default:
+*         description: successful operation
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Storage'
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: ID of the resource
+*/
 router.get("/:id", validateId, getItem);
 
+/**
+* @swagger
+* /storage/{id}:
+*   delete:
+*     summary: Delete a resource by id
+*     security:
+*       - bearerAuth: []
+*     tags: [Storage]
+*     responses:
+*       default:
+*         description: successful operation
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Storage'
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: ID of the resource
+*/
 router.delete("/:id", validateId, deleteItem);
 
 
