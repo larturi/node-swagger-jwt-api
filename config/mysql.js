bca4e507ab99f6d4e0522b23813c21b57e9e0322
Leandro Arturi <lea.arturi@gmail.com>
2022-10-12 13:27:22 -0300

    Docker

--- config/mysql.js
+++ config/mysql.js
@@ -4,9 +4,11 @@ const database = process.env.MYSQL_DATABASE;
 const username = process.env.MYSQL_USER;
 const password = process.env.MYSQL_PASSWORD;
 const host = process.env.MYSQL_HOST;
+const port = process.env.MYSQL_PORT;
 
 const sequelize = new Sequelize(database, username, password, {
   host: host,
+  port: port,
   dialect: "mysql",
 });
 
